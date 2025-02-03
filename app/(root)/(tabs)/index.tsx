23b1b53909a11f6b8b98564fc22371afd361d87e
OMI786I <sohrawardy1998@gmail.com>
2025-02-03 16:32:20 +0600

    implementing homescreen ui

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,16 +1,85 @@
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome!</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      {/**Header */}
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center justify-between">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-xs font-rubik-medium text-black-300">
+                Adrian Hajdin
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+      </View>
+
+      {/**Search */}
+      <View className="px-5">
+        {" "}
+        <Search />
+      </View>
+
+      {/**Featured Section */}
+
+      <View className="my-5 px-5">
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">
+            Featured
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        {/** Featured Cards */}
+
+        <View className="px-5 flex flex-row gap-5 mt-5">
+          <FeaturedCard />
+          <FeaturedCard />
+          <FeaturedCard />
+        </View>
+      </View>
+
+      {/**Recommendation section */}
+
+      <View className="my-5 px-5">
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">
+            Our Recommendation
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <Filters />
+
+        {/**Recommendation Card */}
+
+        <View className="flex flex-row gap-5 mt-5">
+          <Card />
+          <Card />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
